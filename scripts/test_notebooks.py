8433107e0131ff3b0a85cda80af4047cf1e318c6
Charlene Leong <charleneleong84@gmail.com>
2022-02-17 02:35:41 +0000

    Fixing notebook test

--- scripts/test_notebooks.py
+++ scripts/test_notebooks.py
@@ -11,7 +11,7 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 import traceback
 
-from relevanceai.concurrency import multiprocess
+from utils import multiprocess
 
 import logging
 import argparse
